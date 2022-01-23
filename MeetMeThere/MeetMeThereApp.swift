e45440377ea09404a792d98860b9e30fce889783
Maxim Vainikka <maxim.vainikka@yandex.ru>
2022-01-23 13:58:41 +0300

    Initial Commit

---
+++ MeetMeThere/MeetMeThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetMeThereApp.swift
+//  MeetMeThere
+//
+//  Created by Максим Нуждин on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MeetMeThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
